cc13ee6c333798f7ca462535fb55a01d74adcf48
Kenyi Paul <kenyipaul69@gmail.com>
2025-02-26 16:06:28 +0200

    Implemented admin dashboard

---
+++ src/pages/views/dashboard.jsx
@@ -0,0 +1,35 @@
+export default function Dashboard() {
+    return (
+        <div className="dashboard-view">
+
+            <div className="dashboard">
+                <h2>Good Afternoon John!</h2>
+
+                <div className="dash-reports">
+                    <div className="report">
+                        <h1>100</h1>
+                        <p>Registered Users</p>
+                    </div>
+
+                    <div className="report">
+                        <h1>100</h1>
+                        <p>USSD Users</p>
+                    </div>
+
+                    <div className="report">
+                        <h1>100</h1>
+                        <p>Posted Jobs</p>
+                    </div>
+
+                    <div className="report">
+                        <h1>15</h1>
+                        <p>Employers</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    )
+}
