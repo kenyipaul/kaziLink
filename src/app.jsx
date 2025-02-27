e19b1236dc48f9a5ba8ce59d57758d698e7b9510
chiekjowk <chiekjowk@tutamail.com>
2025-02-27 10:36:05 +0200

    -added the create_job pae

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/navbar"
 import Content from "./components/content";
+import CreateJob from "./pages/createJob/createJob";
 import Registration, { EmployersForm, WorkersForm } from "./forms/registration";
 import { createHashRouter, Outlet, RouterProvider } from "react-router-dom";
 
@@ -25,6 +26,11 @@ const router = createHashRouter([
                         element: <EmployersForm />
                     }
                 ]
+            },
+            {
+                path: "/createJob",
+                element: <CreateJob />
+
             }
         ]
     }
---
+++ src/pages/createJob/createJob.jsx
@@ -0,0 +1,7 @@
+export default function CreateJob() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
