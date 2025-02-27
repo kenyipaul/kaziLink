f4df0385cae0e55c49485b57a09cfd281cba0a11
chiekjowk <chiekjowk@tutamail.com>
2025-02-27 08:52:11 +0200

    used sass

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,12 @@
-import Navbar from "./components/Navbar/Navbar.jsx";
-import Content from "./components/content/content.jsx";
+import Navbar from "./components/navbar.jsx";
+// import Content from "./components/content/content.jsx";
 import AdminDashboard from "./pages/admin_dashboard";
 
 export default function App() {
     return (
         <div>
             <Navbar/>
-            <Content/>
+            {/* <Content/> */}
             {/* <AdminDashboard /> */}
         </div>
     )
---
+++ src/components/navbar.jsx
@@ -0,0 +1,24 @@
+import "../sass/navbar.scss"
+
+
+export default function landing(){
+  return (
+    <div className="div">
+
+      <div className="navbar-container">
+        <div className="logo">
+          <img src="/KaziLink 1.png" alt="logo" />
+          <h1>KaziLink</h1>
+        </div>
+
+        <div className="navbar-buttons">
+          <button>Join as Worker</button>
+          <button>Join as Employer</button>
+        </div>
+
+      </div>
+
+    </div>
+  );
+};
+
